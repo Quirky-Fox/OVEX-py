1883ad2c20c647de4042a8c644e69106da34550b
Quirky-Fox <duncan@orbits.co.za>
2022-04-07 20:57:13 +0200

    Add more functions.

--- ovex_python/client.py
+++ ovex_python/client.py
@@ -62,7 +62,7 @@ class Client(BaseClient):
         to_amount = Amount specified in output volume. E.g. (market = btczar, side = buy : to_amount is in btc. If side = sell, to_amount is in zar)
         
 
-        curl --location --request GET 'www.ovex.io/api/v2/rfq/get_quote?market=btczar&from_amount=500000&side=buy&to_amount=1'
+        curl --location --request GET 'www.ovex.io/api/v2/rfq/get_quote'
         """
         market = market.lower()
         side = side.lower()
@@ -81,3 +81,168 @@ class Client(BaseClient):
         return self.do('GET', '/rfq/get_quote', req=req, auth=True)
 
 
+    def get_trades(self, limit=50, order_by='desc', timestamp=None, id_from=None, id_to=None):
+        """
+        Get the RfQ trades,
+
+        ----------------------------------------------------------------
+        
+        limit = Limit the number of returned orders, default to 50. (Optional)
+        order_by = If set, returned rfq trades will be sorted in specific order, default to 'desc'
+        timestamp = An integer represents the seconds elapsed since Unix epoch. If set, only trades executed before the time will be returned.
+        id_from = Trade id. If set, only trades created after the trade will be returned.
+        id_to = Trade id. If set, only trades created before the trade will be returned.
+        
+
+        curl --location --request GET 'www.ovex.io/api/v2/rfq/trades'
+        """
+        
+        req = {
+            'limit': limit,
+            'order_by': order_by,
+            'timestamp': timestamp,
+            'from': id_from,
+            'to': id_to
+        }
+        return self.do('GET', '/rfq/trades', req=req, auth=True)
+    
+    def accept_quote(self,quote_token):
+        """
+        Accept a quote,
+
+        ----------------------------------------------------------------
+        
+        quote_token = Unique token generated from requesting a quote obtained from the get_quote endpoint.
+        
+
+        curl --location --request GET 'www.ovex.io/api/v2/rfq/accept_quote'
+        """
+        
+        req = {
+            'quote_token': quote_token
+        }
+        return self.do('POST', '/rfq/accept_quote', req=req, auth=True)
+    
+    
+    def get_withdraws(self, currency, page=1, limit=100):
+        """
+        Get the withdraws.
+
+        ----------------------------------------------------------------
+        
+        currency = Any supported currencies: zar, btc, eth, tusd, ZAR, BTC, ETH, TUSD.
+        page = Page number (defaults to 1). (Optional)
+        limit = Number of withdraws per page (defaults to 100, maximum is 1000). (Optional)
+
+        curl --location --request GET 'www.ovex.io/api/v2/withdraws'
+        """
+        
+        req = {
+            'currency': currency,
+            'page': page,
+            'limit': limit
+        }
+        return self.do('GET', '/withdraws', req=req, auth=True)
+    
+    def get_deposits(self, currency, limit=None, state=None):
+        """
+        Get the deposits.
+
+        ----------------------------------------------------------------
+        
+        currency = Currency value contains zar, btc, eth, tusd, ZAR, BTC, ETH, TUSD. (Optional)
+        limit = Set result limit. (Optional)
+        state = State of the deposit. (Optional)
+
+        curl --location --request GET 'www.ovex.io/api/v2/deposits'
+        """
+        
+        req = {
+            'currency': currency,
+            'limit': limit,
+            'state': state
+        }
+        return self.do('GET', '/deposits', req=req, auth=True)
+    
+    
+    def get_deposit_info(self, txid):
+        """
+        Get the information about a specific deposit.
+
+        ----------------------------------------------------------------
+        
+        txid = Transaction ID of the deposit
+
+        curl --location --request GET 'www.ovex.io/api/v2/deposit'
+        """
+        
+        req = {
+            'txid': txid
+        }
+        return self.do('GET', '/deposit', req=req, auth=True)
+    
+    def get_deposit_address(self, currency):
+        """
+        Get the deposit address.
+
+        ----------------------------------------------------------------
+        
+        currency = The currency for the deposit address.
+
+        curl --location --request GET 'www.ovex.io/api/v2/deposit_address'
+        """
+        
+        req = {
+            'currency': currency
+        }
+        return self.do('GET', '/deposit_address', req=req, auth=True)
+    
+    
+    
+    def get_currencies(self, type=None):
+        """
+        Get all the currencies and all their information.
+
+        ----------------------------------------------------------------
+        
+        type = 'coin'/'fiat'
+
+        curl --location --request GET 'www.ovex.io/api/v2/currencies'
+        """
+        
+        req = {
+            'type': type
+        }
+        return self.do('GET', '/currencies', req=req, auth=False)
+    
+    
+    def get_currency_info(self, id):
+        """
+        Get the currencies and all their information.
+
+        ----------------------------------------------------------------
+        
+        id = the currency id.
+
+        curl --location --request GET 'www.ovex.io/api/v2/currencies/{id}'
+        """
+        
+        return self.do('GET', f'/currencies/{id}', auth=False)
+    
+    def get_accounts(self, currency=None):
+        """
+        Get the accounts.
+
+        ----------------------------------------------------------------
+        
+        currency = User account currency.
+
+        curl --location --request GET 'www.ovex.io/api/v2/accounts'
+        """
+        
+        req = {
+            'currency': currency
+        }
+        
+        return self.do('GET', '/accounts', req=req, auth=True)
+    
